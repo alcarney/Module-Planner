a7dd5fabceece0808ebec0abcf468e689aa8bb6d
Alex Carney <alex.rugby101@gmail.com>
2014-10-09 16:54:27 +0100

    Added the script that can draw something on screen

---
+++ script/planner.js
@@ -0,0 +1,12 @@
+window.onload = function ()
+{
+    // Load the canvas and get a 2d context
+    var c = document.getElementById("modules");
+    var ctx = c.getContext("2d");
+
+
+}
+
+
+    //ctx.fillStyle = "#FF0000"
+    //ctx.fillRect(0,0,150,75);
