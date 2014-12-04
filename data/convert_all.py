7bdd3d9c79aac5c49298fdc68474b67bb2f63296
Alex Carney <alex.rugby101@gmail.com>
2014-12-04 17:56:07 +0000

    Deleted old data

--- data/convert_all.py
+++
@@ -1,9 +0,0 @@
-"""
-Script that calls `convert_to_md` on every file in this directory (relative paths)
-"""
-from glob import glob
-from subprocess import call
-
-json_files = glob('*.json')
-for f in json_files:
-    call(['python', 'convert_to_md.py', f])
