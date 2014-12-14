b00029c92ec9182e13b16b37013540b6e9bcb0b4
vince <vincent.knight@gmail.com>
2014-12-14 10:50:52 +0000

    Have moved files used for tests

--- bin/check_specifications.py
+++ bin/check_specifications.py
@@ -27,7 +27,7 @@ def read_module(module_file):
     """
     Function to read in module file.
 
-        >>> read_module('module_data_file_test.md')
+        >>> read_module('./files_for_tests/module_data_file_test.md')
         {'code': 'MA0111', 'layout': 'module', 'title': 'Elementary Number Theory I', 'url': 'http://www.cardiff.ac.uk/maths/degreeprogrammes/modules/ma0111.html', 'prequesites': ['None'], 'categories': 'module firstyear'}
 
     """
@@ -41,48 +41,48 @@ def check_module_obeys_specifications(module_file, specifications_file):
     """
     Check that a module obeys the regex of the specifications file
 
-        >>> check_module_obeys_specifications('module_data_file_test.md', 'data_specification.yml')
+        >>> check_module_obeys_specifications('./files_for_tests/module_data_file_test.md', 'data_specification.yml')
         True
 
     Check that doesn't work if incorrect layout is in file (corresponding file has layout 'nonsense'):
 
-        >>> check_module_obeys_specifications('wrong_layout_module_data_file_test.md', 'data_specification.yml')
+        >>> check_module_obeys_specifications('./files_for_tests/wrong_layout_module_data_file_test.md', 'data_specification.yml')
         Traceback (most recent call last):
            ...
-        ValueError: Variable 'layout' in 'wrong_layout_module_data_file_test.md' does not match specification
+        ValueError: Variable 'layout' in './files_for_tests/wrong_layout_module_data_file_test.md' does not match specification
 
     Check that doesn't work if incorrect code is in file (corresponding file has code 'CS011'):
 
-        >>> check_module_obeys_specifications('wrong_code_module_data_file_test.md', 'data_specification.yml')
+        >>> check_module_obeys_specifications('./files_for_tests/wrong_code_module_data_file_test.md', 'data_specification.yml')
         Traceback (most recent call last):
            ...
-        ValueError: Variable 'code' in 'wrong_code_module_data_file_test.md' does not match specification
+        ValueError: Variable 'code' in './files_for_tests/wrong_code_module_data_file_test.md' does not match specification
 
 
 
-        >>> check_module_obeys_specifications('wrong_code_module_data_file_test.md', 'data_specification.yml')
+        >>> check_module_obeys_specifications('./files_for_tests/wrong_code_module_data_file_test.md', 'data_specification.yml')
         Traceback (most recent call last):
            ...
-        ValueError: Variable 'code' in 'wrong_code_module_data_file_test.md' does not match specification
+        ValueError: Variable 'code' in './files_for_tests/wrong_code_module_data_file_test.md' does not match specification
 
     Check that doesn't work if incorrect category is in file (corresponding file has category 'MSc'):
 
-        >>> check_module_obeys_specifications('wrong_category_module_data_file_test.md', 'data_specification.yml')
+        >>> check_module_obeys_specifications('./files_for_tests/wrong_category_module_data_file_test.md', 'data_specification.yml')
         Traceback (most recent call last):
            ...
-        ValueError: Variable 'categories' in 'wrong_category_module_data_file_test.md' does not match specification
+        ValueError: Variable 'categories' in './files_for_tests/wrong_category_module_data_file_test.md' does not match specification
 
     Check that works if correct prerequesites is in file
 
-        >>> check_module_obeys_specifications('correct_prequesites_module_data_file_test.md', 'data_specification.yml')
+        >>> check_module_obeys_specifications('./files_for_tests/correct_prequesites_module_data_file_test.md', 'data_specification.yml')
         True
 
     Check that works if incorrect prerequesites is in file (corresponding file has one prequesites as 'Ice Cream'
 
-        >>> check_module_obeys_specifications('incorrect_prequesites_module_data_file_test.md', 'data_specification.yml')
+        >>> check_module_obeys_specifications('./files_for_tests/incorrect_prequesites_module_data_file_test.md', 'data_specification.yml')
         Traceback (most recent call last):
            ...
-        ValueError: Variable 'prequesites' in 'incorrect_prequesites_module_data_file_test.md' does not match specification
+        ValueError: Variable 'prequesites' in './files_for_tests/incorrect_prequesites_module_data_file_test.md' does not match specification
 
     """
     module_specs = read_module(module_file)
